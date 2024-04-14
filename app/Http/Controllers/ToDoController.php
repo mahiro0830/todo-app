4c47e7d5bdac0dd91ba0a4f5907bb058399ba638
峯岸真弘 <shishisus4@gmail.com>
2024-04-14 23:42:35 +0900

    todo登録用のstoreメソッド作成

--- app/Http/Controllers/ToDoController.php
+++ app/Http/Controllers/ToDoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ToDo\StoreRequest;
 use App\Models\ToDo;
 use Illuminate\Http\Request;
 
@@ -37,9 +38,16 @@ class ToDoController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreRequest $request)
     {
-        //
+        // 新規のToDoモデルを作成する
+        $toDo = new ToDo();
+
+        // タイトルをToDoモデルに設定する
+        $toDo->title = $request->get('title');
+
+        // DBにデータを登録する
+        $toDo->save();
     }
 
     /**
